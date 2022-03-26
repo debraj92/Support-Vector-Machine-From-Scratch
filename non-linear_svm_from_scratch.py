eae8ed75296e84b75182a014c80c05b3c5742d26
Debraj Ray <debrajray@Debrajs-MacBook-Pro.local>
2022-03-26 15:07:23 -0600

    RBF kernel impl

--- non-linear_svm_from_scratch.py
+++ non-linear_svm_from_scratch.py
@@ -5,7 +5,7 @@ from scipy.optimize import Bounds, BFGS
 from scipy.optimize import LinearConstraint, minimize
 
 """
-Implementation of Non-Linear SVM with polynomial kernel.
+Implementation of Non-Linear SVM with polynomial and gaussian (rbf) kernel.
 """
 
 
@@ -16,6 +16,13 @@ class MySVM_PolynomialKernel:
     w_opt = -1
     b_opt = -1
     alpha = []
+    KernelFunction = None
+
+    def __init__(self, kernel="poly"):
+        if kernel.lower() == "poly":
+            self.KernelFunction = self.polynomialKernelTransform
+        elif kernel.lower() == "rbf":
+            self.KernelFunction = self.gaussianKernelTransform
 
     def lagrange_dual(self, alpha, x, t):
         """
@@ -43,11 +50,26 @@ class MySVM_PolynomialKernel:
         ind_sv = np.where(alpha > self.ZERO)[0]
         for i in ind_sv:
             for k in ind_sv:
-                result = result + alpha[i] * alpha[k] * t[i] * t[k] * self.polyKernelTransform(x[i, :], x[k, :])
+                result = result + alpha[i] * alpha[k] * t[i] * t[k] * self.KernelFunction(x[i, :], x[k, :])
         result = 0.5 * result - sum(alpha)
         return result
 
-    def polyKernelTransform(self, X, Z):
+    def gaussianKernelTransform(self, X, Z):
+        """
+        The Kernel function for gaussian (RBF) transformation is:
+
+        exp(−gamma * |x−z|^2)
+
+        where gamma is a hyper-parameter. I have set it to 0.1.
+
+        :param X:
+        :param Z:
+        :return:
+        """
+        gamma = 0.1
+        return np.exp(-gamma * np.linalg.norm(X - Z) ** 2)
+
+    def polynomialKernelTransform(self, X, Z):
         """
         The Kernel function for polynomial transformation is:
         (1 + x.z)^n
@@ -114,7 +136,7 @@ class MySVM_PolynomialKernel:
         for j in range(n):
             p = b
             for i in range(m):
-                p += alpha[i] * t[i] * self.polyKernelTransform(x[i, :], x_test[j, :])
+                p += alpha[i] * t[i] * self.KernelFunction(x[i, :], x_test[j, :])
 
             y_test.append(p)
         return np.sign(y_test)
@@ -122,9 +144,9 @@ class MySVM_PolynomialKernel:
     def calculate_b_optimum(self, alpha, t, x):
         """
         b* = average(t - (w* . f(x)))
-        b* = average(t - (∑ αi*ti*f(x).f(x)))
+        b* = average(t - (∑ αi*ti*f(xi).f(x)))
 
-        We can apply the kernel trick the calculate f(x).f(x) without explicitly transforming x to f(x)
+        We can apply the kernel trick the calculate f(xi).f(x) without explicitly transforming x to f(x)
 
         :param alpha: alpha
         :param t: target
@@ -138,7 +160,7 @@ class MySVM_PolynomialKernel:
         for s in ind_sv:
             w = 0
             for i in range(m):
-                w = w + alpha[i] * t[i] * self.polyKernelTransform(x[i, :], x[s, :])
+                w = w + alpha[i] * t[i] * self.KernelFunction(x[i, :], x[s, :])
             w0 = w0 + t[s] - w
         # Take the average
         w0 = w0 / len(ind_sv)
@@ -198,12 +220,13 @@ class MySVM_PolynomialKernel:
                         hue=labels, markers=['s', 'P'],
                         palette=['red', 'black'])
 
-    def plotTrueFunction(self):
+    def plotTruePolyFunction(self):
         """
         This plots ths true non-linear function which is a 5th degree polynomial. The svm's task is to approximate this function
         as much as possible. It achieves 100% accuracy on the test set with a 3rd degree polynomial transformation.
         :return:
         """
+
         def getTrueY(x):
             return 3 * pow(x, 5) + 4 * pow(x, 4) + 7 * pow(x, 3) - 9 * pow(x, 2) + 1
 
@@ -217,36 +240,79 @@ class MySVM_PolynomialKernel:
         plt.plot(x, y)
 
 
-svm2 = MySVM_PolynomialKernel()
+def executeExamplePolynomialSVM():
+    svm2 = MySVM_PolynomialKernel("poly")
 
-x = np.array(
-    [[-0.5, 0], [-0.7, -2], [0, 1.2], [0.3, 0.9], [0.7, 3], [-0.5, 0], [0.5, 0.3], [-0.6, -1], [0, 0.5], [0.3, 0],
-     [0.4, -0.5], [1, 1], [1, 2], [-0.1, -0.5], [-0.3, -0.6], [-0.4, -3]])
-labels = np.array([1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1])
+    x = np.array(
+        [[-0.5, 0], [-0.7, -2], [0, 1.2], [0.3, 0.9], [0.7, 3], [-0.5, 0], [0.5, 0.3], [-0.6, -1], [0, 0.5], [0.3, 0],
+         [0.4, -0.5], [1, 1], [1, 2], [-0.1, -0.5], [-0.3, -0.6], [-0.4, -3]])
+    labels = np.array([1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1])
 
-# Plot the training data along with the true function
-svm2.plotTrueFunction()
-svm2.plot_x_train(x, labels)
-plt.savefig('training-data-non-linear-svm.png')
-plt.clf()
+    # Plot the training data along with the true function
+    svm2.plotTruePolyFunction()
+    svm2.plot_x_train(x, labels)
+    plt.savefig('training-data-polynomial-svm.png')
+    plt.clf()
 
-x_test = np.array(
-    [[2, 2], [-0.5, 2], [0.5, -2], [-2, 0.5], [0, 0], [0.3, 1.2], [0, 0.3], [0.1, 1.3], [0.6, 0.4], [0.6, -0.4],
-     [0.7, 4], [-2, -3]])
-labels_test = np.array([-1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1])
+    x_test = np.array(
+        [[2, 2], [-0.5, 2], [0.5, -2], [-2, 0.5], [0, 0], [0.3, 1.2], [0, 0.3], [0.1, 1.3], [0.6, 0.4], [0.6, -0.4],
+         [0.7, 4], [-2, -3]])
+    labels_test = np.array([-1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, 1])
 
-# Plot the testing data along with the true function
-svm2.plotTrueFunction()
-svm2.plot_x_test(x_test, labels_test)
-plt.savefig('testing-data-non-linear-svm.png')
+    # Plot the testing data along with the true function
+    svm2.plotTruePolyFunction()
+    svm2.plot_x_test(x_test, labels_test)
+    plt.savefig('testing-data-polynomial-svm.png')
 
-C = 100
-# SVM trains using the training data and corresponding target values in a supervised fashion.
-svm2.train(x, labels, C)
+    C = 100
+    # SVM trains using the training data and corresponding target values in a supervised fashion.
+    svm2.train(x, labels, C)
 
-# SVM inference uses the test data. It does not use labels_test which is the expected class of the test data samples.
-y_test = svm2.classify_points(svm2.alpha, labels, x, x_test)
+    # SVM inference uses the test data. It does not use labels_test which is the expected class of the test data samples.
+    y_test = svm2.classify_points(svm2.alpha, labels, x, x_test)
 
-# Once prediction is obtained from SVM, we measure the accuracy using labels of the test data.
-print("Accuracy on test set ",svm2.accuracy(y_test, labels_test))
+    # Once prediction is obtained from SVM, we measure the accuracy using labels of the test data.
+    print("Accuracy on test set of Polynomial SVM ", svm2.accuracy(y_test, labels_test))
 
+
+def executeExampleRBFSVM():
+    svm2 = MySVM_PolynomialKernel("RBF")
+
+    x = np.array(
+        [[2.8, 2], [3.1, 4], [-1, 4], [-0.5, 1], [1, 1], [1, 5], [1.8, 1.3], [2.9, 4.5], [1.7, 5.3], [-1.3, 1.6],
+         [-1.5, 2.6], [1, 3], [0.5, 2.5], [0.8, 3.5], [1.6, 3.5], [1.5, 2.9], [0.2, 3], [0.2, 3.5], [1.7, 2.5]])
+    labels = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1])
+
+    # Plot the training data along with the true function
+    circle = plt.Circle((1, 3), 1.732)
+    fig, ax = plt.subplots()
+    ax.add_patch(circle)
+    svm2.plot_x_train(x, labels)
+    plt.savefig('training-data-rbf-svm.png')
+    plt.clf()
+
+    x_test = np.array(
+        [[0.4, 3.6], [1, 4], [1.3, 2.4], [2, 3], [0.4, 2.9], [-1, 1.4], [2.5, 5], [2.2, 1.4], [-0.9, 4.6], [1, 0.2],
+         [4, 3], [-3, 1]])
+    labels_test = np.array([-1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1])
+
+    # Plot the testing data along with the true function
+    circle = plt.Circle((1, 3), 1.732)
+    fig, ax = plt.subplots()
+    ax.add_patch(circle)
+    svm2.plot_x_test(x_test, labels_test)
+    plt.savefig('testing-data-rbf-svm.png')
+
+    C = 100
+    # SVM trains using the training data and corresponding target values in a supervised fashion.
+    svm2.train(x, labels, C)
+
+    # SVM inference uses the test data. It does not use labels_test which is the expected class of the test data samples.
+    y_test = svm2.classify_points(svm2.alpha, labels, x, x_test)
+    # Once prediction is obtained from SVM, we measure the accuracy using labels of the test data.
+    print("Accuracy on test set of RBF SVM", svm2.accuracy(y_test, labels_test))
+
+
+executeExamplePolynomialSVM()
+
+executeExampleRBFSVM()
